1e2304bb5a235e174cffecc8c49ab9d3f2574a7b
CodeCaspian <122201295+CodeCaspian@users.noreply.github.com>
2023-01-08 17:40:36 +0530

    Update SVM.py

--- Python ML/SVM.py
+++ Python ML/SVM.py
@@ -3,32 +3,22 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 
 df = pd.read_csv('Position_Salaries.csv') 
- 
 x = df.iloc[:, 1:2].values 
 y = df.iloc[:,2:].values
 
 from sklearn.model_selection import train_test_split 
-
-# 70% training and 30% test 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3,random_state=109) 
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3,random_state=None) 
 
 from sklearn import svm 
 
 clf = svm.SVC(kernel='linear') # Linear Kernel
-
-#Train the model using the training sets 
 clf.fit(X_train, y_train) 
 
-            #OR
-            
+#OR           
 # from sklearn.svm import SVR 
 # regressor = SVR(kernel = 'rbf') 
 
-#Predict the response for test dataset 
 y_pred = clf.predict(X_test) 
 
-#Import scikit-learn metrics module for accuracy calculation 
 from sklearn import metrics 
-
-# Model Accuracy: how often is the classifier correct? 
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
