3bc1d4c96182fd10695153226df321082e2459e5
CodeCaspian <122201295+CodeCaspian@users.noreply.github.com>
2023-01-08 17:42:28 +0530

    Update SVM.py

--- Python ML/SVM.py
+++ Python ML/SVM.py
@@ -10,13 +10,12 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3,random_state=None) 
 
 from sklearn import svm 
-
 clf = svm.SVC(kernel='linear') # Linear Kernel
 clf.fit(X_train, y_train) 
 
 #OR           
 # from sklearn.svm import SVR 
-# regressor = SVR(kernel = 'rbf') 
+# clf = SVR(kernel = 'rbf') 
 
 y_pred = clf.predict(X_test) 
 
