674d376e5439808e8ec1ed3e5ad8b89d6b0790ca
CodeCaspian <122201295+CodeCaspian@users.noreply.github.com>
2023-01-09 23:34:53 +0530

    Update MultiReg.py

--- Python ML/MultiReg.py
+++ Python ML/MultiReg.py
@@ -4,7 +4,6 @@ import pandas as pd
  
 data_set= pd.read_csv('Startups.csv')  
 
-#Extracting Independent and dependent Variable  
 x= data_set.iloc[:, :-1].values  
 y= data_set.iloc[:, 4].values
 
@@ -14,16 +13,12 @@ labelencoder_x = LabelEncoder()
 x[:, 3] = labelencoder_x.fit_transform(x[:,3])  
 ct = ColumnTransformer([("Countries", OneHotEncoder(), [3])], remainder = 'passthrough')
 x = ct.fit_transform(x)
- 
 x = x[:, 1:]  
-print('New',x)
-# If we do not remove the first dummy variable, then it may introduce multicollinearity in the model.
 
 # Splitting the dataset into training and test set.  
 from sklearn.model_selection import train_test_split  
 x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.2, random_state=0)  
-
-#Fitting the MLR model to the training set:  
+ 
 from sklearn.linear_model import LinearRegression  
 regressor = LinearRegression()  
 regressor.fit(x_train, y_train)  
